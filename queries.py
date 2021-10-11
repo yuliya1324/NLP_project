17cbff6a1565cad71da8222b2467c546a8f768c1
yuliya1324 <koylenka15@gmail.com>
2021-10-11 17:28:18 +0300

    added search function and comments

--- queries.py
+++ queries.py
@@ -1,10 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask
+import re
+import spacy
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///base.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+nlp_spacy = spacy.load("ru_core_news_sm")
+db = SQLAlchemy()
 
 
 class Lemmas(db.Model):
@@ -46,7 +45,13 @@ class Words(db.Model):
     word = db.Column('word', db.Text)
 
 
-def find_parses(type_query, query):
+def find_parses(type_query: str, query: str) -> list:
+    '''
+    Функция, которая по запросу находит id разборов
+    :param type_query: тип запроса: {pos, word, lemma, lemma+pos}
+    :param query: строка запроса
+    :return: список id разборами
+    '''
     if type_query == 'pos':
         return [parse.id for parse in
                 Parses.query.join(Poses, Poses.id == Parses.id_pos).filter(Poses.pos == query).all()]
@@ -57,12 +62,19 @@ def find_parses(type_query, query):
         return [parse.id for parse in
                 Parses.query.join(Lemmas, Lemmas.id == Parses.id_lemma).filter(Lemmas.lemma == query).all()]
     elif type_query == 'lemma+pos':
+        lemma, pos = query.split('+')
         return [parse.id for parse in Parses.query
                 .join(Lemmas, Lemmas.id == Parses.id_lemma).join(Poses, Poses.id == Parses.id_pos)
-                .filter(Lemmas.lemma == 'сидеть').filter(Poses.pos == 'VERB').all()]
+                .filter(Lemmas.lemma == lemma).filter(Poses.pos == pos).all()]
 
 
-def select_unigram(type_query, query):
+def select_unigram(type_query: str, query: str) -> list:
+    '''
+    Функция, которая выдает вхождения запроса (униграммы)
+    :param type_query: тип запроса: {pos, word, lemma, lemma+pos}
+    :param query: строка запроса
+    :return: список кортежей с местоположением каждого вхождения запроса: (id_doc, id_sent, position)
+    '''
     parses = find_parses(type_query, query)
     result = []
     i = 0
@@ -78,17 +90,67 @@ def select_unigram(type_query, query):
     return result
 
 
-def select_bigram(type_query_1, query_1, type_query_2, query_2):
-    result_1 = select_unigram(type_query_1, query_1)
-    result_2 = select_unigram(type_query_2, query_2)
-    return [x for x in result_1 if (x[0], x[1], x[2] + 1) in result_2]
+def select(params: list) -> list:
+    '''
+    Функция, которая выдает вхождения запроса (униграммы/биграммы/триграммы)
+    :param params: список кортежей с типом запроса и самой строкой запроса
+    :return: список кортежей с местоположением каждого вхождения запроса: (id_doc, id_sent, position)
+    '''
+    result_1 = select_unigram(params[0][0], params[0][1])
+    if len(params) == 1:
+        return result_1
+    elif len(params) == 2:
+        result_2 = select_unigram(params[1][0], params[1][1])
+        return [x for x in result_1 if (x[0], x[1], x[2] + 1) in result_2]
+    else:
+        result_2 = select_unigram(params[1][0], params[1][1])
+        result_3 = select_unigram(params[2][0], params[2][1])
+        return [x for x in result_1 if (x[0], x[1], x[2] + 1) in result_2 and (x[0], x[1], x[2] + 2) in result_3]
 
 
-def select_trigram(type_query_1, query_1, type_query_2, query_2, type_query_3, query_3):
-    result_1 = select_unigram(type_query_1, query_1)
-    result_2 = select_unigram(type_query_2, query_2)
-    result_3 = select_unigram(type_query_3, query_3)
-    return [x for x in result_1 if (x[0], x[1], x[2] + 1) in result_2 and (x[0], x[1], x[2] + 2) in result_3]
+def process_item(item: str) -> tuple or False:
+    '''
+    Функция, которая обрабатывает запрос и выдает либо его тип и его самого в нужном формате,
+    либо то, что такой запрос невалидный
+    :param item: строка с запросом
+    :return: кортеж с типом запроса и самим запросом или False
+    '''
+    poses = ['ADJ', 'ADP', 'ADV', 'AUX', 'INTJ', 'CCONJ', 'X', 'NOUN',
+             'DET', 'PROPN', 'NUM', 'VERB', 'PART', 'PRON', 'SCONJ', 'SYM']
+    if re.search(r'^"\w+"$', item):
+        type_query = 'word'
+        item = item.replace('"', '')
+    elif item in poses:
+        type_query = 'pos'
+    elif re.search(r'^\w+\+[A-Z]+$', item):
+        if re.search(r'^\w+\+([A-Z]+)$', item).group(1) in poses:
+            type_query = 'lemma+pos'
+        else:
+            return False
+    elif re.search(r'^\w+$', item):
+        type_query = 'lemma'
+        item = nlp_spacy(item)[0].lemma_
+    else:
+        return False
+    return type_query, item
 
 
-print(select_trigram('pos', 'PROPN', 'pos', 'VERB', 'pos', 'NOUN')[:20])
+def process_query(search: str) -> list or False:
+    '''
+    Функция, которая по строке запроса выдает все его вхождения
+    :param search: строка запроса
+    :return: список кортежей с местоположением каждого вхождения запроса: (id_doc, id_sent, position)
+    '''
+    if not search:
+        return False
+    search = search.split()
+    if len(search) > 3:
+        return False
+    params = []
+    for item in search:
+        type_item, item = process_item(item)
+        if type_item:
+            params.append((type_item, item))
+        else:
+            return False
+    return select(params)
